1dfada431b5148aa7cf1121f0d3b168edd75d8a0
mehulchau <mehulcm17@gmail.com>
2023-01-18 20:21:06 -0800

    Parsed megamap .txt files and created map with target gene as key and array of affected genes. modified:   package-lock.json modified:   package.json modified:   src/App.css modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+//import Ideogram from "ideogram";
 import './App.css';
 
-function App() {
+function MyComponent() {
+  const [selectedValue, setSelectedValue] = useState("");
+  const [megaMap, setMegaMap] = useState({});
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const handleFileSelect = async (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const text = e.target.result;
+      const lines = text.split("\n");
+
+      // Create an array names the first element for each line
+      const newMegaMap = {};
+      lines.forEach((line) => {
+        const [key, ...values] = line.split("\t");
+        newMegaMap[key] = values;
+      });
+      setMegaMap(newMegaMap);
+    };
+    await reader.readAsText(file);
+  };
+  
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    setIsButtonClicked(true);
+    console.log(megaMap[selectedValue]);
+    // call a function to update the annotation on the ideogram
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="heading">Ideogram Annotations</h1>
+      <div className="content">
+        <div className="topContent">
+          <input type="file" onChange={handleFileSelect} />
+          <select value={selectedValue} onChange={handleChange}>
+            {Object.keys(megaMap).map((key) => (
+              <option key={key} value={key}>
+                {key}
+              </option>
+            ))}
+          </select>
+          <button onClick={handleClick}>Display Ideogram</button>
+        </div>
+        <div className="middleContent">
+          {isButtonClicked ? 
+          <div>
+            <h2 className="displayHeading">All affected genes</h2>
+            <p className="affectedGeneList">{JSON.stringify(megaMap[selectedValue], null, 2)}</p>
+          </div>
+          : null}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default MyComponent;
